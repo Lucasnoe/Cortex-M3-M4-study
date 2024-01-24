11bd3eceb65876ec3c1120c62d913ac3916dc698
Lucas Noé <lucas.nooe@outlook.com>
2024-01-23 22:00:18 -0300

    trace with ITM added

--- core_registers/Src/main.c
+++ core_registers/Src/main.c
@@ -17,6 +17,7 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -25,5 +26,10 @@
 int main(void)
 {
     /* Loop forever */
-	for(;;);
+	for(;;)
+	{
+		printf("Hello World!\n");
+
+	}
+
 }
